296484766d15b0bad33be4b19a7a6a8b457c52ad
Joshua Hatzinger <joshua.hatzinger@stud.tu-darmstadt.de>
2024-06-24 15:23:50 +0200

    Init py and setter

---
+++ custom_components/huawei_lte/__init__.py
@@ -0,0 +1,31 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
+
+CODEOWNERS = ["@YoshiiPlayzz"]
+
+DEPENDENCIES = ['network']
+AUTO_LOAD = ['sensor']
+
+CONF_HUAWEI_LTE_SENSOR_ID = 'huawei_lte_sensor_id'
+CONF_HOST = 'host'
+CONF_USERNAME = 'username'
+CONF_PASSWORD = 'password'
+
+huawei_lte_ns = cg.esphome_ns.namespace('huawei_lte')
+HuaweiLTEComponent = huawei_lte_ns.class_('HuaweiLTE', cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(HuaweiLTEComponent),
+    cv.Required(CONF_HOST): cv.string,
+    cv.Required(CONF_USERNAME): cv.string,
+    cv.Required(CONF_PASSWORD): cv.string,
+}).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    cg.add(var.set_host(config[CONF_HOST]))
+    cg.add(var.set_username(config[CONF_USERNAME]))
+    cg.add(var.set_password(config[CONF_PASSWORD]))
+
